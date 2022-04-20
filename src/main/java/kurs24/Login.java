5f3706bdbba2eb9326d8b107bbe5654fedf733fd
Valeeerie <valeria180501@mail.ru>
2022-04-20 13:40:16 +0400

    логин приобрел логику

--- src/main/java/kurs24/Login.java
+++ src/main/java/kurs24/Login.java
@@ -1,54 +1,54 @@
 package kurs24;
 
-import java.io.IOException;
+    import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+    import javax.servlet.RequestDispatcher;
+    import javax.servlet.ServletException;
+    import javax.servlet.annotation.WebServlet;
+    import javax.servlet.http.HttpServlet;
+    import javax.servlet.http.HttpServletRequest;
+    import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name="Login", urlPatterns="/Login")
-public class Login extends HttpServlet {
+    import kurs24.CalcSum.RequestCalc;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    @WebServlet(name="Login", urlPatterns="/Login")
+    public class Login extends HttpServlet {
 
-		LoginCheck log =LoginCheck.fromRequestParameters(request);
-		log.setAsRequestAttributesAndCheck(request);
-			RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
-			dispatcher.forward(request, response);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
 
-}
-	
-	private static class LoginCheck{
-		private String login;
-		private String password;
-		private String result;
-		
-		private LoginCheck(String login, String password){
-		this.login=login;
-		this.password=password;
-		}
-		
-		public static LoginCheck fromRequestParameters(HttpServletRequest request) {
-			return new LoginCheck(
-					request.getParameter("login"),
-					request.getParameter("password"));	
-		}
-		
-		public void setAsRequestAttributesAndCheck(HttpServletRequest request) {
-			request.setAttribute("login", login);
-			request.setAttribute("password", password);
-			String first_try;
-			int second_try;
-				first_try=login;
-				second_try=Integer.parseInt(password);
-				if (first_try=="hi" | second_try==1234) {
-				result= "Успех";}
-					
-			request.setAttribute("result", result);
-		}
-		
-	}
-	}
+    response.getWriter().append("Served at: ").append(request.getContextPath());
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+
+    int login;
+    int password;
+
+    try {
+    login = Integer.parseInt(request.getParameter("login"));
+    password = Integer.parseInt(request.getParameter("password"));
+    }
+    catch (NumberFormatException e) {
+    login=310302;
+    password=310302;
+    }
+
+    if (login == 123 || password == 123) {
+    RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
+    dispatcher.forward(request, response);
+    login = 0;
+    password = 0;
+    }
+    else {
+    RequestDispatcher dispatcher = request.getRequestDispatcher("/Login.jsp");
+    dispatcher.forward(request, response);
+    login = 0;
+    password = 0;
+    }
+    }
+
+    }
+
+Ещё
