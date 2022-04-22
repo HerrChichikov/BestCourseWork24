01fec125e06c03c59e2a6c1047541037671c99be
Saetgareev <103118875+Saetgareev@users.noreply.github.com>
2022-04-23 00:31:05 +0400

    БОЖЕСТВЕННАЯ регистрация завершена

--- src/main/java/kurs24/Login.java
+++ src/main/java/kurs24/Login.java
@@ -1,10 +1,12 @@
 package kurs24;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -17,13 +19,15 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "Login", urlPatterns = "/Login")
 public class Login extends HttpServlet {
-	 String last;
+	String last;
 
-	/*protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}*/
+	/*
+	 * protected void doGet(HttpServletRequest request, HttpServletResponse
+	 * response) throws ServletException, IOException {
+	 * 
+	 * response.getWriter().append("Served at: ").append(request.getContextPath());
+	 * }
+	 */
 
 	public class User {
 		private String password;
@@ -46,6 +50,7 @@ public class Login extends HttpServlet {
 		}
 
 	}
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		User user = new User();
@@ -53,34 +58,49 @@ public class Login extends HttpServlet {
 		user.setPassword(request.getParameter("password"));
 		user.getLogin();
 		user.getPassword();
-		
+		String filepath = new File("").getCanonicalPath();
+		String[] parsfilepath = filepath.split("/");
+		int lengthpath = parsfilepath.length;
+		String abspath = "";
+		for (int i = 0; i < (lengthpath - 1); i++) {
+			abspath = abspath + parsfilepath[i] + "/";
+		}
+		filepath = abspath + "webapps/Calculate/logs.txt";
+		File file = new File(filepath);
+		FileInputStream fis = new FileInputStream(file);
+		InputStreamReader isr = new InputStreamReader(fis, "utf-8");
+		BufferedReader br = new BufferedReader(isr);
+		String line = "";
 		String login = user.getLogin();
 		String password = user.getPassword();
+		int b;
+		b = 0;
 		request.setCharacterEncoding("UTF-8");
-		if (login.equals("123") && password.equals("123")) {
-			request.getRequestDispatcher("/index.jsp").forward(request, response);
-			login = "0";
-			password = "0";
-		} else {
-			RequestDispatcher dispatcher = request.getRequestDispatcher("/Login.jsp");
-			dispatcher.forward(request, response);
-			login = "0";
-			password = "0";
+		while ((line = br.readLine()) != null) {//// Прочтите каждую строку br и обновляйте строку каждый раз
+			if (line.equals("un" + login)) {// Находим правильное имя пользователя
+
+				if (br.readLine().equals("pw" + password)) {// Получив правильное имя пользователя, оцениваем
+															// правильность пароля
+					request.setAttribute("massage", "");
+					request.getRequestDispatcher("/index.jsp").forward(request, response);					
+					b = 1;
+					break;
+				} else {
+					// String path=request.getContextPath()+"/Login.jsp";
+					// response.sendRedirect(path);
+					request.setAttribute("massage", "Неверное имя пользователя или пароль");
+					request.getRequestDispatcher("/Login.jsp").forward(request, response);
+
+					break;
+				}
+			}
 		}
-	}}
-		
-		
 
-//		try {
-//			login = Integer.parseInt(request.getParameter("login"));
-//			password = Integer.parseInt(request.getParameter("password"));
-//		} catch (NumberFormatException e) {
-//			login = 310302;
-//			password = 310302;
-//		}
-//
-		
-	
+		// br.close();
+		if (b == 0) {
+			request.setAttribute("massage", "Неверное имя пользователя или пароль");
+			request.getRequestDispatcher("/Login.jsp").forward(request, response);
 
-
-
+		}
+	}
+}
